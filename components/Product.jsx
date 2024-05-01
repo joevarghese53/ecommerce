a5babf7679bd02e94eb2a19e502b64deacdc28a2
Joe Varghese <joevarghese53@gmail.com>
2024-05-01 10:44:02 +0530

    product card

--- components/Product.jsx
+++ components/Product.jsx
@@ -16,6 +16,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
               className="product-image"
             />
             <p className="product-name">{name}</p>
+            <p className="product-cat">Oversized T-Shirts</p>
             <p className="product-price">₹{price}</p>
           </div>
         </Link>
